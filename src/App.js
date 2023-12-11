20473894ad382bfdc9ff7e87fc359920b3e03303
Mahak Taneja <tanejamehak39@gmail.com>
2023-12-11 17:44:43 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="Walk" />
+      <Todo text="Read" />
+      <Todo text="Dance" />
+   
+    </div>
+  );
+}
+
+export default App;
